1453e88123368c7cf5f045fc08a91b10eea06375
virajrai <virajrai97@gmail.com>
2016-09-30 16:55:44 -0400

    Update solution.java

--- solution.java
+++ solution.java
@@ -9,6 +9,16 @@
  */
 public class Solution {
     public int maxDepth(TreeNode root) {
-        
+        if (root == null)
+            return 0;
+        else{
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            
+            if (leftDepth >= rightDepth)
+                return leftDepth + 1;
+            else
+                return rightDepth + 1;
+        }
     }
 }
